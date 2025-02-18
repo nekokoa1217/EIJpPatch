fba8b87285b2fb7e022fb2b7ad6cbceef7879101
nekokoa1217 <yako.kurama@gmail.com>
2025-02-18 19:31:51 +0900

    リファクタリング

--- Launcher_JP.bat
+++ Launcher_JP.bat
@@ -1,6 +1,5 @@
 @echo off
 chcp 65001
-set gitRepository="xxxx"
 set jpLang_folder=".\jp"
 set usLang_folder=".\us"
 set destination_folder="..\Game\Gamedata\localized\us"
@@ -9,29 +8,22 @@ cd "../"
 start "" ".\LauncherIcarus.exe"
 cd "./jpLocal"
 
-echo 日本語ファイルをダウンロードしています...
-xcopy %gitRepository%"\*" "%jpLang_folder%\" /E /H /C /I /Y /Q
-echo ダウンロードが完了しました。
-
 echo 英語ファイルを初期化しています...
 xcopy %usLang_folder%"\*" "%destination_folder%\" /E /H /C /I /Y /Q
-echo 英語ファイルの初期化が完了しました。
+
 
 :loop
 tasklist /FI "IMAGENAME eq Launcher.exe" 2>NUL | find /I "Launcher.exe" >NUL
 if "%ERRORLEVEL%"=="0" (
-    echo ICARUSの起動を確認。
-    echo 英語ファイルのバックアップをします。
+    echo 英語ファイルのバックアップ中...
     xcopy "%destination_folder%\*" "%usLang_folder%\" /E /H /C /I /Y /Q
-    echo 日本語ファイルを適用します。
+    echo 日本語ファイルを適用中...
     xcopy "%jpLang_folder%\*" "%destination_folder%\" /E /H /C /I /Y /Q
-    echo 日本語ファイルを適用が完了しました。
     goto end
 ) else (
     timeout /T 5 >NUL
     goto loop
 )
-
 
 :end
 echo 5秒後にウィンドウが閉じます...
