b9299d6210be751c2fc57c19af7d626974b91f84
nekokoa1217 <yako.kurama@gmail.com>
2025-02-18 20:12:42 +0900

    リファクタ

--- Launcher_JP.bat
+++ Launcher_JP.bat
@@ -4,21 +4,24 @@ set jpLang_folder=".\jp"
 set usLang_folder=".\us"
 set destination_folder="..\Game\Gamedata\localized\us"
 
+if not exist %usLang_folder% mkdir %usLang_folder%
+
+set current_folder=%cd%
+
 cd "../"
 start "" ".\LauncherIcarus.exe"
-cd "./jpLocal"
+cd %current_folder%
 
 echo 英語ファイルを初期化しています...
-xcopy %usLang_folder%"\*" "%destination_folder%\" /E /H /C /I /Y /Q
-
+xcopy %usLang_folder%"\*" "%destination_folder%\" /E /H /C /I /Y
 
 :loop
 tasklist /FI "IMAGENAME eq Launcher.exe" 2>NUL | find /I "Launcher.exe" >NUL
 if "%ERRORLEVEL%"=="0" (
     echo 英語ファイルのバックアップ中...
-    xcopy "%destination_folder%\*" "%usLang_folder%\" /E /H /C /I /Y /Q
+    xcopy "%destination_folder%\*" "%usLang_folder%\" /E /H /C /I /Y
     echo 日本語ファイルを適用中...
-    xcopy "%jpLang_folder%\*" "%destination_folder%\" /E /H /C /I /Y /Q
+    xcopy "%jpLang_folder%\*" "%destination_folder%\" /E /H /C /I /Y
     goto end
 ) else (
     timeout /T 5 >NUL
